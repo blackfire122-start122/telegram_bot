ab1091c598f0472d0a8770ad0625efe99e576af9
Igor <gsambir519@gmail.com>
2021-09-28 20:38:58 +0300

    install mysql

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
-from config import TOKEN
 
+from config import TOKEN
+import markup as nav
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
@@ -12,39 +12,78 @@ urgent_list = []
 temporary_list = []
 longterm_list = []
 
-name_shop = ""
+type_shop_to_ask = False
+name_to_ask = False
+name_shop_ask = False
+
 type_shop = ""
 name = ""
 
-btn_add = KeyboardButton("add shop")
-start_menu = ReplyKeyboardMarkup().add(btn_add)
-
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await bot.send_message(message.from_user.id,'Hello',reply_markup=start_menu)
+	global name_to_ask
+	if not name_to_ask:
+		await bot.send_message(message.from_user.id,'Привіт твоє імя')
+		name_to_ask = True
 
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
-    await message.reply("Цей бот буде показувати список покупок,які потрібно купити їх є три види:\n\n1 Термінові\n2 Тимчасові\n3 Довгосрочні")
+	await message.reply("Цей бот буде показувати список покупок, які\
+		потрібно купити їх є три види:\n\n1 Термінові - urgent\n2 Тимчасові-temporary_list\n3 Довгосрочні-longterm_list")
 
-@dp.message_handler(commands=['add_shop'])
+@dp.message_handler(commands=['shop'])
 async def help_command(message: types.Message):
-    await message.reply("name shop")
-
+	global shop_to_ask
+	await bot.send_message(message.from_user.id,"what shop",reply_markup=nav.shop_menu)
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
 	global name
-	if not name:
+	global name_to_ask,type_shop_to_ask,name_shop_ask
+	global name_shop,type_shop
+	if name_to_ask and not name:
 		name = msg.text
-		print(name)
-	if not name_shop:
-		name_shop = msg.text
-		print(name)
-	if not type_shop:
+		print("new user",name)
+
+	elif msg.text == "add shop":
+		type_shop_to_ask = True
+		await bot.send_message(msg.from_user.id,"тип покупки",reply_markup=nav.shop_type)
+	
+	elif msg.text == "wath all shop":
+			all_shop = "urgent\n\t•"+"\n\t•".join(urgent_list)+"\ntemporary\n\t•"+"\n\t•".join(temporary_list)+"\nlong-term\n\t•"+"\n\t•".join(longterm_list)
+			await bot.send_message(msg.from_user.id,all_shop)
+
+	elif msg.text == "wath urgent shop":
+			all_shop = "urgent\n\t•"+"\n\t•".join(urgent_list)
+			await bot.send_message(msg.from_user.id,all_shop)
+
+	elif msg.text == "wath temporary shop":
+			all_shop = "temporary\n\t•"+"\n\t•".join(temporary_list)
+			await bot.send_message(msg.from_user.id,all_shop)
+
+	elif msg.text == "wath long-term shop":
+			all_shop = "long-term\n\t•"+"\n\t•".join(longterm_list)
+			await bot.send_message(msg.from_user.id,all_shop)
+
+	elif type_shop_to_ask:
 		type_shop = msg.text
-		print(name)
-    #await bot.send_message(msg.from_user.id, msg.text)
+		type_shop_to_ask = False
+		name_shop_ask = True
+		await bot.send_message(msg.from_user.id,"покупка")
+
+
+	elif name_shop_ask:
+		if type_shop =="urgent":
+			urgent_list.append(msg.text+" -- "+name)
+		elif type_shop=="temporary":
+			temporary_list.append(msg.text+name)
+		elif type_shop=="long-term":
+			longterm_list.append(msg.text+name)
+		else:
+			pass
+		name_shop_ask = False
+		print("new shop",name,type_shop)
+		await bot.send_message(msg.from_user.id,"покупку " + msg.text + " додано як " + type_shop + "")
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+	executor.start_polling(dp)
---
+++ markup.py
@@ -0,0 +1,23 @@
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
+
+btn_name = KeyboardButton("Your name")
+start_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_name)
+
+btn_add = KeyboardButton("add shop")
+btn_wath_all_shop = KeyboardButton("wath all shop")
+btn_wath_urgent_shop = KeyboardButton("wath urgent shop")
+btn_wath_temporary_shop = KeyboardButton("wath temporary shop")
+btn_wath_longterm_shop = KeyboardButton("wath long-term shop")
+shop_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_wath_urgent_shop,
+											btn_wath_temporary_shop,
+											btn_wath_longterm_shop
+).add(btn_add).add(btn_wath_all_shop)
+
+btn_urgent_shop = KeyboardButton("urgent")
+btn_temporary_shop = KeyboardButton("temporary")
+btn_longterm_shop = KeyboardButton("long-term")
+shop_type = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_urgent_shop,
+											btn_temporary_shop,
+											btn_longterm_shop
+)
+
