037045ca9a00f2ffb1da449a8da5318cbe2b05ae
Igor <gsambir519@gmail.com>
2021-09-29 12:38:55 +0300

    beta version

--- bot.py
+++ bot.py
@@ -15,37 +15,63 @@ db = mysql.connector.connect(
 	database="shop_list",
 )
 
-cursor = db.cursor()
+cursor = db.cursor(buffered=True)
+
 # cursor.execute("CREATE DATABASE shop_list")
-# cursor.execute("CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), user_id INT UNIQUE)")
+# cursor.execute("CREATE TABLE urgent_shop (urgent_shop VARCHAR(255), username VARCHAR(255))")
+# cursor.execute("CREATE TABLE temporary_shop (temporary_shop VARCHAR(255), username VARCHAR(255))")
+# cursor.execute("CREATE TABLE longterm_shop (longterm_shop VARCHAR(255), username VARCHAR(255))")
 
-
-# sql = "FROM users SELECT "
+# sql = "TRUNCATE TABLE users"
 # cursor.execute(sql)
 # db.commit()
-# print(cursor.rowcount)
+
+# sql = "TRUNCATE TABLE urgent_shop"
+# cursor.execute(sql)
+# db.commit()
+
+# sql = "TRUNCATE TABLE temporary_shop"
+# cursor.execute(sql)
+# db.commit()
+
+# sql = "TRUNCATE TABLE longterm_shop"
+# cursor.execute(sql)
+# db.commit()
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-urgent_list = []
-temporary_list = []
-longterm_list = []
-
 type_shop_to_ask = False
 name_to_ask = False
 name_shop_ask = False
+delete_shop_to_ask = False
+name_delete_shop_to_ask = False
 
+send_mes_shop_add = True
 type_shop = ""
 name = ""
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-	global name_to_ask
-	if not name_to_ask:
+	global name, name_to_ask
+	try:
+		sql = "SELECT * FROM users WHERE user_id = " + str(message.from_user.id) + ";"
+		cursor.execute(sql)
+		db.commit()
+	except:
+		pass
+	data_req = cursor.fetchone()
+	if not data_req:
+		id = 0
+	else:
+		id = data_req[2]
+	if not name_to_ask and id !=message.from_user.id:
 		await bot.send_message(message.from_user.id,'Привіт твоє імя')
 		name_to_ask = True
-
+	else:
+		name = data_req[1]
+		await bot.send_message(message.from_user.id,'Ви вже зареєстровані як '+name, reply_markup=nav.shop_menu)
+		
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
 	await message.reply("Цей бот буде показувати список покупок, які\
@@ -59,57 +85,144 @@ async def help_command(message: types.Message):
 @dp.message_handler()
 async def echo_message(msg: types.Message):
 	global name
-	global name_to_ask,type_shop_to_ask,name_shop_ask
-	global name_shop,type_shop
+	global name_to_ask,type_shop_to_ask,name_shop_ask,delete_shop_to_ask,name_delete_shop_to_ask
+	global name_shop,type_shop,send_mes_shop_add
 	if name_to_ask and not name:
 		name = msg.text
-
-		sql = "INSERT INTO users (username,user_id) VALUES('" + name + "', " + str(msg.from_user.id) + ")"
-		cursor.execute(sql)
-		db.commit()
-		await bot.send_message(msg.from_user.id,'реєстрація пройшла успішно')
-
+		try:
+			sql = "INSERT INTO users (username,user_id) VALUES('" + name + "', " + str(msg.from_user.id) + ")"
+			cursor.execute(sql)
+			db.commit()
+			await bot.send_message(msg.from_user.id,'Реєстрація пройшла успішно',reply_markup=nav.shop_menu)
+		except:
+			await bot.send_message(msg.from_user.id,'Ви вже зареєстровані',reply_markup=nav.shop_menu)
 		print("new user",name)
+	elif not name:
+		await bot.send_message(msg.from_user.id,'Зареєструйтесь',reply_markup=nav.btn_start_main)
 
 	elif msg.text == "add shop":
 		type_shop_to_ask = True
 		await bot.send_message(msg.from_user.id,"тип покупки",reply_markup=nav.shop_type)
 	
+	elif msg.text == "delete shop":
+		delete_shop_to_ask = True
+		await bot.send_message(msg.from_user.id,"тип видаляємої покупки",reply_markup=nav.shop_type)
+	
 	elif msg.text == "wath all shop":
-			all_shop = "urgent\n\t•"+"\n\t•".join(urgent_list)+"\ntemporary\n\t•"+"\n\t•".join(temporary_list)+"\nlong-term\n\t•"+"\n\t•".join(longterm_list)
-			await bot.send_message(msg.from_user.id,all_shop)
+		try:
+			sql = "SELECT * FROM urgent_shop;"
+			cursor.execute(sql)
+			db.commit()
+			data_req = cursor.fetchall()
+		except:
+			pass
+		all_shop = "urgent\n"
+		for i in data_req:
+			all_shop+='\t• '+i[0]+'--'+i[1]+"\n"
+		try:
+			sql = "SELECT * FROM temporary_shop;"
+			cursor.execute(sql)
+			db.commit()
+			data_req = cursor.fetchall()
+		except:
+			pass
+		all_shop += "temporary\n"
+		for i in data_req:
+			all_shop+='\t• '+i[0]+'--'+i[1]+"\n"
+		try:
+			sql = "SELECT * FROM longterm_shop;"
+			cursor.execute(sql)
+			db.commit()
+			data_req = cursor.fetchall()
+		except:
+			pass
+		all_shop += "longterm\n"
+		for i in data_req:
+			all_shop+='\t• '+i[0]+'--'+i[1]+"\n"
+
+		await bot.send_message(msg.from_user.id,all_shop)
 
 	elif msg.text == "wath urgent shop":
-			all_shop = "urgent\n\t•"+"\n\t•".join(urgent_list)
-			await bot.send_message(msg.from_user.id,all_shop)
+		try:
+			sql = "SELECT * FROM urgent_shop;"
+			cursor.execute(sql)
+			db.commit()
+			data_req = cursor.fetchall()
+		except:
+			pass
+		urgent_shop = "urgent\n"
+		for i in data_req:
+			urgent_shop+='\t• '+i[0]+'--'+i[1]+"\n"
+		await bot.send_message(msg.from_user.id,urgent_shop)
 
 	elif msg.text == "wath temporary shop":
-			all_shop = "temporary\n\t•"+"\n\t•".join(temporary_list)
-			await bot.send_message(msg.from_user.id,all_shop)
+		try:
+			sql = "SELECT * FROM temporary_shop;"
+			cursor.execute(sql)
+			db.commit()
+			data_req = cursor.fetchall()
+		except:
+			pass
+		temporary_shop = "temporary\n"
+		for i in data_req:
+			temporary_shop+='\t• '+i[0]+'--'+i[1]+"\n"
+		await bot.send_message(msg.from_user.id,temporary_shop)
 
 	elif msg.text == "wath long-term shop":
-			all_shop = "long-term\n\t•"+"\n\t•".join(longterm_list)
-			await bot.send_message(msg.from_user.id,all_shop)
+		try:
+			sql = "SELECT * FROM longterm_shop;"
+			cursor.execute(sql)
+			db.commit()
+			data_req = cursor.fetchall()
+		except:
+			pass
+		longterm_shop = "longterm\n"
+		for i in data_req:
+			longterm_shop+='\t• '+i[0]+'--'+i[1]+"\n"
+		await bot.send_message(msg.from_user.id,longterm_shop)
 
 	elif type_shop_to_ask:
 		type_shop = msg.text
 		type_shop_to_ask = False
 		name_shop_ask = True
-		await bot.send_message(msg.from_user.id,"покупка")
+		await bot.send_message(msg.from_user.id,"Імя покупки")
 
+	elif delete_shop_to_ask:
+		delete_shop_to_ask = False
+		name_delete_shop_to_ask = True
+		type_shop = msg.text
+		await bot.send_message(msg.from_user.id,"Імя видаляємої покупки")
 
 	elif name_shop_ask:
-		if type_shop =="urgent":
-			urgent_list.append(msg.text+" -- "+name)
-		elif type_shop=="temporary":
-			temporary_list.append(msg.text+name)
-		elif type_shop=="long-term":
-			longterm_list.append(msg.text+name)
-		else:
-			pass
+		send_mes_shop_add = True
+		try:
+			sql = "INSERT INTO "+type_shop+"_shop ("+type_shop+"_shop,username) VALUES('" + msg.text + "', '" + name + "')"
+			cursor.execute(sql)
+			db.commit()
+		except Exception as e:
+			print(e)
+			await bot.send_message(msg.from_user.id,"Покупку " + msg.text + "не додано помилка")
+			send_mes_shop_add = False
 		name_shop_ask = False
-		print("new shop",name,type_shop)
-		await bot.send_message(msg.from_user.id,"покупку " + msg.text + " додано як " + type_shop + "")
+		if send_mes_shop_add:
+			print("new shop",msg.text,name,type_shop)
+			await bot.send_message(msg.from_user.id,"Покупку " + msg.text + " додано як " + type_shop + "",reply_markup=nav.shop_menu)
+
+	elif name_delete_shop_to_ask:
+		name_delete_shop_to_ask = False
+		try:
+			sql = "DELETE FROM "+type_shop+"_shop WHERE "+type_shop+"_shop = '"+msg.text+"'"
+			cursor.execute(sql)
+			db.commit()
+		except Exception as e:
+			print(e)
+			await bot.send_message(msg.from_user.id,"Покупку " + msg.text + "не видалено помилка")
+			send_mes_shop_add = False
+		if cursor.rowcount:
+			print("Покупку " + msg.text + " видалено" + name)
+			await bot.send_message(msg.from_user.id,"Покупку " + msg.text + " видалено ", reply_markup=nav.shop_menu)
+		else:
+			await bot.send_message(msg.from_user.id,"Покупку " + msg.text + " не знайдено ", reply_markup=nav.shop_menu)
 
 if __name__ == '__main__':
 	executor.start_polling(dp)
--- markup.py
+++ markup.py
@@ -4,6 +4,7 @@ btn_name = KeyboardButton("Your name")
 start_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_name)
 
 btn_add = KeyboardButton("add shop")
+btn_delete = KeyboardButton("delete shop")
 btn_wath_all_shop = KeyboardButton("wath all shop")
 btn_wath_urgent_shop = KeyboardButton("wath urgent shop")
 btn_wath_temporary_shop = KeyboardButton("wath temporary shop")
@@ -11,13 +12,15 @@ btn_wath_longterm_shop = KeyboardButton("wath long-term shop")
 shop_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_wath_urgent_shop,
 											btn_wath_temporary_shop,
 											btn_wath_longterm_shop
-).add(btn_add).add(btn_wath_all_shop)
+).add(btn_add,btn_delete).add(btn_wath_all_shop)
 
 btn_urgent_shop = KeyboardButton("urgent")
 btn_temporary_shop = KeyboardButton("temporary")
-btn_longterm_shop = KeyboardButton("long-term")
+btn_longterm_shop = KeyboardButton("longterm")
 shop_type = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_urgent_shop,
 											btn_temporary_shop,
 											btn_longterm_shop
 )
 
+btn_start = KeyboardButton("/start")
+btn_start_main = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_start)
